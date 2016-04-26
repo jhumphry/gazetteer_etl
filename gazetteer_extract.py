657bb196c7df13180ee6a6464834bee0f65944d5
James Humphry <git@binecho.co.uk>
2016-04-26 20:48:40 +0100

    Correct handling of text file types
    We do not know what the appropriate text encoding is, so all files
    should be initially opened as binary and only wrapped into a TextIo
    when the table has been identified and the encoding is known.

--- gazetteer_extract.py
+++ gazetteer_extract.py
@@ -148,15 +148,7 @@ file_ext = os.path.splitext(args.file)[1]
 
 tables_modified = []
 
-if file_ext == '.txt' or file_ext == '.csv':
-    with open(args.file, 'rt') as fp, \
-            connection.cursor() as cur:
-
-        tables_modified.append(process_file(args.file, fp, cur))
-
-    connection.commit()
-
-if file_ext == '.dbf':
+if file_ext in ('.txt', '.csv', '.dbf'):
     with open(args.file, 'rb') as fp, \
             connection.cursor() as cur:
 
